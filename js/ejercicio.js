b2a6b39252f032976460e9a0028d405842bdd13c
natuchasca <natuxalee@gmail.com>
2016-06-20 13:47:16 -0400

    trabajando funciones

---
+++ js/ejercicio.js
@@ -0,0 +1,74 @@
+// EJERCICIOS FUNCIONES
+// Quitar comentarios para visualizar uno a uno
+
+
+/*
+//#1 EL MENOR DE LOS NUMEROS
+function numeroMenor (n1, n2, n3) {
+  var n1, n2, n3;
+
+  if ((n1 <= n2) && (n1 <= n3)) {
+  return("el numero menor es " + n1);
+  }
+  else if  ((n2 <= n1) && (n2 <= n3)) {
+  return("el numero menor es " + n2);
+  }
+  else {
+  return("el numero menor es " + n3);
+  }
+}
+*/
+
+
+/*
+//#2 NUMERO PAR O IMPAR
+function numeropar (n1) {
+  if (n1%2==0) {
+    return("El número ingresado es PAR");
+  }
+  else {
+    return("El número ingresado es IMPAR");
+  }
+}
+*/
+
+
+//#3 ORDENANDO NUMEROS
+
+
+
+
+//#4 MAYUSCULAS O MINUSCULAS
+
+
+
+
+//#5 PALINDROMO
+
+
+
+
+//#6 OPERACIONES MATEMATICAS
+
+
+
+
+//#7 CUADRADO DE LA SUMA DE 2 NUMEROS
+
+
+
+
+//#8 NUMERO PRIMO
+
+
+
+
+//#9 MULTIPLOS DE UN NUMERO
+
+
+
+
+//#10 CANTIDAD DE DIGITOS 
+
+
+
