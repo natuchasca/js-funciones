af20d20632538b9879ba5b94b946a5c0dd353f12
natuchasca <natuxalee@gmail.com>
2016-06-22 12:21:22 -0400

    Trabajando aun...

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -3,7 +3,7 @@
 
 
 /*
-//#1 EL MENOR DE LOS NUMEROS
+//#1 EL MENOR DE LOS NUMEROS  /    OK!
 function numeroMenor (n1, n2, n3) {
   var n1, n2, n3;
 
@@ -21,7 +21,7 @@ function numeroMenor (n1, n2, n3) {
 
 
 /*
-//#2 NUMERO PAR O IMPAR
+//#2 NUMERO PAR O IMPAR  /    OK!
 function numeropar (n1) {
   if (n1%2==0) {
     return("El número ingresado es PAR");
@@ -32,10 +32,12 @@ function numeropar (n1) {
 }
 */
 
-
+/*
 //#3 ORDENANDO NUMEROS
+function mayorMenor (n1, n2, n3) {
 
-
+}
+*/
 
 
 //#4 MAYUSCULAS O MINUSCULAS
@@ -47,23 +49,66 @@ function numeropar (n1) {
 
 
 
+/*
+//#6 OPERACIONES MATEMATICAS  /    OK!
+function suma (a,b) {
+  var suma = a + b;
+  return("La suma de " + a + " y " + b + " es " + suma)
+}
+function resta (a,b) {
+  var resta = a - b;
+  return("La resta de " + a + " y " + b + " es " + resta)
+}
+function multiplica (a,b) {
+  var multiplica = a * b;
+  return("La multiplicación de " + a + " y " + b + " es " + multiplica)
+}
+function divide (a,b) {
+  var divide = a / b;
+  return("La division de " + a + " y " + b + " es " + divide)
+}
+*/
 
-//#6 OPERACIONES MATEMATICAS
 
+/*
+//#7 CUADRADO DE LA SUMA DE 2 NUMEROS  /    OK!
+function cuadrado (a, b) {
+  var suma = a + b;
+  var cuadrado = suma * suma;
+  return("El cuadrado de la suma de " + a + " y " + b + " es " + cuadrado);
+}
+*/
 
-
-
-//#7 CUADRADO DE LA SUMA DE 2 NUMEROS
-
-
-
-
+/*
 //#8 NUMERO PRIMO
+function primo (n) {
+
+}
+
+if ((n/n=parseInt) && (n/1=parseInt)) {
+  return("El numero es primo");
+ }
+ else {
+  return("El numero no es primo");
+ }
+*/
 
 
+//#9 MULTIPLOS DE UN NUMERO  / NOP algo falta...
+function multiplos (numero) {
+  multiplos = 0;
+  for (i = 1 ; i < 11; i++) {
+    multiplos = (numero)*i; 
+    if(numero==2) {
+    return("El número es primo");
+    }
+    else {
+    return("El número no es primo");
+    }
+  }
+};
 
 
-//#9 MULTIPLOS DE UN NUMERO
 
 
 
